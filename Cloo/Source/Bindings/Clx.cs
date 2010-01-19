09531d29616bc74a706aae780ea39f8159ca1eb5
nythrix <nythrix@users.sourceforge.net>
2010-01-19 20:23:43 +0100

    Added a new class for compute extensions.

---
+++ Cloo/Source/Bindings/Clx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Cloo.Bindings
+{
+    public enum ComputeGLContextInfo: int
+    {
+    }
+
+    public class Clx
+    {
+        public class Delegates
+        {
+            public unsafe delegate Int32 clGetGLContextInfoKHR(
+                IntPtr* properties,
+                ComputeGLContextInfo param_name,
+                IntPtr param_value_size,
+                IntPtr param_value,
+                IntPtr* param_value_size_ret );
+        }
+
+        public readonly static Delegates.clGetGLContextInfoKHR GetGLContextInfoKHR;
+
+        static Clx()
+        {
+            GetGLContextInfoKHR = ( Delegates.clGetGLContextInfoKHR )Marshal.GetDelegateForFunctionPointer(
+                Imports.GetExtensionFunctionAddress( "clGetGLContextInfoKHR" ),
+                typeof( Delegates.clGetGLContextInfoKHR ) );
+        }
+    }
+}
