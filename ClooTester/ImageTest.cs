9de766ec3757762adb253eb551349d0a7cb67938
nythrix <nythrix@users.sourceforge.net>
2010-01-19 20:28:34 +0100

    Added a new test for image support.

---
+++ ClooTester/ImageTest.cs
@@ -0,0 +1,15 @@
+﻿using Cloo;
+using System;
+namespace ClooTester
+{
+    public class ImageTest: AbstractTester
+    {
+        public ImageTest()
+            : base( "Image Test" )
+        { }
+
+        protected override void RunInternal()
+        {
+        }
+    }
+}
