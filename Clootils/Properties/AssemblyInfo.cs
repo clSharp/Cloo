adf18b975110af7e06aebacec04ab225f2c78442
nythrix <nythrix@users.sourceforge,net>
2010-03-13 11:07:52 +0100

    Updated documentation and bumped version number for the 0.7.0 release.

--- Clootils/Properties/AssemblyInfo.cs
+++ Clootils/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "0.6.2" )]
-[assembly: AssemblyFileVersion( "0.6.2" )]
+[assembly: AssemblyVersion( "0.7.0" )]
+[assembly: AssemblyFileVersion( "0.7.0" )]
