f3fdd68bd5bff8992d844aa9fef711b673d055b0
Alex Hildebrand <alexander.hildebrand@gmail.com>
2015-02-17 10:23:35 +0100

    Added ComputeExternalEvent for events created by an external library

---
+++ Cloo/Source/ComputeExternalEvent.cs
@@ -0,0 +1,15 @@
+﻿using Cloo.Bindings;
+
+namespace Cloo
+{
+    /// <summary>
+    /// Represents an event created by an external library
+    /// </summary>
+    public class ComputeExternalEvent : ComputeEvent
+    {
+        public ComputeExternalEvent(CLEventHandle handle, ComputeCommandQueue queue)
+            : base(handle, queue)
+        {
+        }
+    }
+}
