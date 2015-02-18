c5e93b370075bc999e54f48eef6a6a4066a091b4
Alex Hildebrand <alexander.hildebrand@gmail.com>
2015-02-18 11:59:33 +0100

    Updated assembly info

--- Cloo/Properties/AssemblyInfo.cs
+++ Cloo/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "Cloo" )]
-[assembly: AssemblyDescription( "Compute Language - Object Oriented" )]
+[assembly: AssemblyDescription( "Compute Language - Object Oriented (clSharp fork)" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany("Fatjon Sakiqi, Alexander Hildebrand")]
 [assembly: AssemblyProduct( "Cloo" )]
 [assembly: AssemblyCopyright( "Copyright (c) 2009 - 2013 Fatjon Sakiqi" )]
 [assembly: AssemblyTrademark( "" )]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyVersion("0.9.1.*")]
 
 [assembly: CLSCompliant( true )]
-[assembly: AssemblyFileVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1.*")]
